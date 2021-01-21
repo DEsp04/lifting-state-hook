30a9f07fa625a26eea985311780bfa402ac32b9e
david espinal <despinal0425@gmail.com>
2021-01-20 19:59:59 -0500

    complete it the code along

---
+++ src/components/FruitList.js
@@ -0,0 +1,16 @@
+
+
+const FruitList = props => { 
+  
+  console.log(props, "FruitList js")
+  
+  return (
+    <ul>
+      {props.fruits.map((fruit) => (
+        <li>{ fruit}</li>
+      ))}
+    </ul>
+  )
+}
+
+export default FruitList;
